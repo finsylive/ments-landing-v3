d4ff624191f26198c241299636e9007c95f9c30f
devotedayush <23f1001477@ds.study.iitm.ac.in>
2025-06-23 14:15:19 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "ments - Where Hustle Begins",
   description: "Join the community where your entrepreneurial journey starts",
-  generator: "v0.dev",
+  icons: { icon: "/icon.png" }
 }
 
 export default function RootLayout({
